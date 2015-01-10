94be5a2d0020be758d0d20bd421f5b02a19bf210
NGUYENDUCTHANH <nguyenducthanhst16@gmail.com>
2015-01-10 08:35:44 +0700

    prepare for pull request

--- client/templates/application/home.js
+++ client/templates/application/home.js
@@ -4,10 +4,15 @@ Template.home.events({
 		var randomString=Math.random().toString(36).substring(2,7);
 		var defaultUrl='http://localhost:3000/';
 		var shortUrl=defaultUrl+randomString;
+		var longUrl=$(e.target).find('#input1').val();
 		var urlItem={
-			longUrl:$(e.target).find('#input1').val(),
+			longUrl:longUrl,
 			shortUrl:shortUrl
 		};
-		urlItem._id=UrlList.insert(urlItem);
+		if(longUrl){
+			urlItem._id=UrlList.insert(urlItem);
+			$('#input1').val('');
+		}
+		
 	}
 });
