a61f3c3e48987621c04bc93d8ec379c7b43b9b0c
NGUYENDUCTHANH <nguyenducthanhst16@gmail.com>
2015-01-16 17:23:27 +0700

    Move important code from client to lib & use Meteor method

--- client/templates/application/home.js
+++ client/templates/application/home.js
@@ -1,32 +1,18 @@
 Template.home.events({
   'submit form': function(e) {
-    var randomString,
-      defaultUrl,
-      shortUrl,
-      longUrl,
-      urlItem;
     e.preventDefault();
-    function checkUnique() {
-      function makeRandomString() {
-        return Math.random().toString(36).substring(2,7);
-      }
-      var a = makeRandomString();
-      while ( UrlList.find({_id:a}).fetch().length ) {
-        a = makeRandomString();
-      }
-      return a;
-    }
-    randomString = checkUnique();
-    defaultUrl = 'http://localhost:3000/';
-    shortUrl = defaultUrl + randomString;
-    longUrl = $(e.target).find('#long-url-input').val();
-    urlItem = {
-      _id: randomString,
-      longUrl: longUrl,
-      shortUrl: shortUrl
+    var longUrl = $(e.target).find('#long-url-input').val();
+    var hostName = location.protocol + '//' + window.location.hostname;
+    var urlAttribute = {
+      hostName: hostName,
+      longUrl: longUrl
     };
+    Meteor.call( 'urlInsert', urlAttribute, function(error, result) {
+      if ( error ) {
+        return alert( error.reason );
+      }
+    });
     if ( longUrl ) {
-      UrlList.insert( urlItem );
       $('#long-url-input').val('');
     }
   }
