cd7464e316ef7dd0581f5fe3f5eae89aba6e3917
NGUYENDUCTHANH <nguyenducthanhst16@gmail.com>
2015-01-17 16:01:52 +0700

    Move Meteor method code to server folder

--- client/templates/application/home.js
+++ client/templates/application/home.js
@@ -2,18 +2,17 @@ Template.home.events({
   'submit form': function(e) {
     e.preventDefault();
     var longUrl = $(e.target).find('#long-url-input').val();
-    var hostName = location.protocol + '//' + window.location.hostname;
     var urlAttribute = {
-      hostName: hostName,
       longUrl: longUrl
     };
+
     Meteor.call( 'urlInsert', urlAttribute, function(error, result) {
       if ( error ) {
         return alert( error.reason );
       }
+      if ( longUrl ) {
+        $('#long-url-input').val('');
+      }
     });
-    if ( longUrl ) {
-      $('#long-url-input').val('');
-    }
   }
 });
---
+++ server/methods.js
@@ -0,0 +1,29 @@
+Meteor.methods({
+  urlInsert: function ( urlAttribute ) {
+    var randomString,
+     shortUrl;
+    function checkUnique() {
+      function makeRandomString() {
+        return Math.random().toString(36).substring(2,7);
+      }
+      var a = makeRandomString();
+      while ( UrlList.find({_id:a}).fetch().length ) {
+        a = makeRandomString();
+      }
+      return a;
+    }
+
+    randomString = checkUnique();
+    shortUrl = Meteor.absoluteUrl() + randomString;
+
+    urlItem = {
+      _id: randomString,
+      longUrl: urlAttribute.longUrl,
+      shortUrl:shortUrl 
+    };
+
+    if ( urlAttribute.longUrl ) {
+      UrlList.insert( urlItem );
+    }
+  }
+});
