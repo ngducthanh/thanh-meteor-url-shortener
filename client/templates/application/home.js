7cc155e77f2c1a3605b2cdba3baf3ca0af76d846
NGUYENDUCTHANH <nguyenducthanhst16@gmail.com>
2015-01-20 09:24:54 +0700

    Create custom short link

--- client/templates/application/home.js
+++ client/templates/application/home.js
@@ -1,13 +1,19 @@
 Template.home.events({
   'submit form': function(e) {
     e.preventDefault();
-    var longUrl = $(e.target).find('#long-url-input').val();
+    var longUrl = $(e.target).find('#long-url-input').val(),
+        customUrl = $(e.target).find('#custom-url-input').val(),
+        urlInput = {
+          longUrl: longUrl,
+          customUrl: customUrl
+        };
 
-    Meteor.call('urlInsert', longUrl, function(error, result) {
+    Meteor.call('urlInsert', urlInput, function(error, result) {
       if (error) {
         return alert(error.reason);
       } else  {
         $('#long-url-input').val('');
+        $('#custom-url-input').val('');
       }
     });
   }
--- server/methods.js
+++ server/methods.js
@@ -1,12 +1,28 @@
 Meteor.methods({
-  urlInsert: function(longUrl) {
-    var shortUrl = Random.id(5),
-        urlItem = {
-          longUrl: longUrl,
-          shortUrl: shortUrl 
-        };
+  urlInsert: function(urlInput) {
+    var shortUrl,
+        urlItem;
 
-    if (longUrl) {
+    if (urlInput.customUrl) {
+      var a = urlInput.customUrl;
+      if (!UrlList.find({
+          shortUrl: a
+        }).fetch().length) {
+        shortUrl = a;
+      } else {
+        console.log('Please create another custom link!');
+        return;
+      }
+    } else {
+      shortUrl = Random.id(5);
+    }
+
+    urlItem = {
+      longUrl: urlInput.longUrl,
+      shortUrl: shortUrl 
+    };
+
+    if (urlInput.longUrl) {
       UrlList.insert(urlItem);
     }
   }
